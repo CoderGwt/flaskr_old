0c38a0a536ce8c42c34832022aedf3551b42d6dc
weitao <1341680843@qq.com>
2018-06-21 19:51:31 +0800

    创建数据库

--- flaskr.py
+++ flaskr.py
@@ -47,6 +47,15 @@ def close_db(error):
         g.sqlite_db.close()
 
 
+def init_db():
+    """初始化数据库"""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", 'r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
