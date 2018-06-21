f6662355fa3efff44aee0f6d3d9da9ca3f07e677
weitao <1341680843@qq.com>
2018-06-21 19:57:53 +0800

    添加显示条目的视图

--- flaskr.py
+++ flaskr.py
@@ -56,6 +56,13 @@ def init_db():
         db.commit()
 
 
+@app.route("/")
+def show_entries():
+    cur = g.db.excute("select title, text from entries order by id desc")
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template("show_entries.html", entries=entries)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
