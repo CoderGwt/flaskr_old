6ad66f6cdd77b21f7f17149ea4c0724f0987a449
weitao <1341680843@qq.com>
2018-06-21 19:37:36 +0800

    添加应用设置

--- flaskr.py
+++ flaskr.py
@@ -1,12 +1,41 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def index():
+#     return "Hello world"
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+# TODO all the imports
+import os
+import sqlite3
+from flask import Flask, request, url_for, session, g, redirect, abort, render_template, flash
 
-app = Flask(__name__)
+app = Flask(__name__)  # create the application instance
+app.config.from_object(__name__)  # load config from this file, flaskr.py
+
+# load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, "flaskr.db"),
+    ECRET_KEY='this is the secret key',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar("FLASK_SETTINGS", silent=True)
 
 
-@app.route("/")
-def index():
-    return "Hello world"
+def connect_db():
+    """connects to the specific databases"""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
